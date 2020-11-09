14bdd3754cf0773b9209e9beb0a341a14fb8f120
Peter Vu <pvu@stripe.com>
2020-11-08 23:02:01 -0800

    Updated proxy path to correctly proxy to image api

--- server/server.js
+++ server/server.js
@@ -1,17 +1,38 @@
+const proxy = require('express-http-proxy');
+const newrelic = require('newrelic');
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 80;
-// const PORT = 3999;
+const imageModule = process.env.IMAGE_URL;
 
-//middleware to redirect requests to root to productId of P001
+//middleware to redirect requests to root to productId of 1
 app.use((req, res, next) => {
   if (req.url === '/') {
-    res.redirect(301, req.url + '?productId=P001');
+    res.redirect(301, req.url + '?productId=1');
   }
   next();
 });
+
+// Picture Service Proxy Here (Port 3001)
+app.use('/photos', proxy(imageModule, {
+  proxyReqPathResolver: function proxyReq(req) {
+    const parts = req.url.split('/');
+    const productId = parts[1];
+    const pictureId = parts[2];
+
+    console.log(parts);
+
+    if (productId === undefined || productId === 0) {
+      return `${imageModule}/photos/1`;
+    } else if (pictureId === undefined) {
+      return `${imageModule}/photos/${productId}`;
+    }
+    return `${imageModule}/photos/${productId}/${pictureId}`;
+  },
+}));
 
 app.use(express.static(path.resolve(__dirname, '../client')));
 
---
+++ server/serverGetTest.js
@@ -0,0 +1,32 @@
+/* eslint-disable import/no-unresolved */
+import http from 'k6/http';
+import { check } from 'k6';
+import { Rate } from 'k6/metrics';
+
+const serverUrl = 'http://127.0.0.1:3000';
+
+const errorRate = new Rate('errorRate');
+
+export const options = {
+  duration: '1s',
+  vus: 50,
+};
+
+export default function runGetRequestTest() {
+  // tests the last 10% of the 10 million products in the database;
+  const randomProductId = Math.floor(Math.random() * 1000000) + 9000000;
+  const response = http.get(`${serverUrl}/photos/${randomProductId}`);
+
+  errorRate.add(response.status !== 200);
+
+  check(response, {
+    'Response code was 200': (res) => {
+      console.log('Response status', res.status);
+      return res.status === 200;
+    },
+    'Product Id exists': (res) => {
+      const { product_id: productId } = res.json();
+      return productId;
+    },
+  });
+}
